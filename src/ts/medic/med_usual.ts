aafeea8026d3b5b8dd6661d48dd32ddd1b316b10
popibeatrice <popicabeatrice31@gmail.com>
2023-02-26 21:56:46 +0200

    vreau signup

--- src/ts/medic/med_usual.ts
+++ src/ts/medic/med_usual.ts
@@ -1,9 +1,3 @@
 // password conffirmation
 
 ///variables
-const MedForm= document.querySelector(".med_singup-form") as HTMLFormElement;
-const Password = new Array();
-const parola = document.querySelector("#pass");
-parola.addEventListener("keyup", () =>{
-    console.log(MedForm.pass.value); 
-})
