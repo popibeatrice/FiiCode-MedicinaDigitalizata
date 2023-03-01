9ac21f82f0d1551093b15af43598802a7d40e19a
DavidPav3l <david_marian@outlook.com>
2023-03-01 21:40:55 +0200

    acum stim si ce medic a invitat pe cine fiecare cu medicutu lui

--- src/ts/pacient/patient_signupUI.js
+++ src/ts/pacient/patient_signupUI.js
@@ -12,15 +12,20 @@ import {
   query,
   documentId,
   getDocs,
+  getDoc,
 } from "firebase/firestore";
 import { iti, UIfollow } from "./intl-tel-inpt";
-
+var telefon;
 // sunt mandru de acest async function
 async function getQuery(q) {
   const querySnapshot = await getDocs(q);
   if (querySnapshot.empty) throw TypeError("nu esti smecher");
 }
-
+async function getDuery(medicRef) {
+  const docSnap = await getDoc(medicRef);
+  if (docSnap.exists()) return docSnap.data().medic;
+  else throw TypeError("nu esti docos");
+}
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -35,6 +40,7 @@ form.addEventListener("submit", (e) => {
   );
   getQuery(q).then(() => {
     {
+      telefon = iti.getNumber();
       phoneSignIn(iti.getNumber());
     }
   });
@@ -61,22 +67,78 @@ function phoneSignIn(phoneNumber) {
       UIfollow();
       // variabile
       const form = document.querySelector("form");
+      const passRef = document.querySelector("#pass");
+      const passConf = document.querySelector("#passconf");
+      const PasswordField = documet.querySelector(".password-regex");
+      const passRegEx = /^(.{0,7}|[^a-z]{1,}|[^A-Z]{1,}|[^\d]{1,})$|[\s]/;
+      // pass conf
+      passRef.addEventListener("keyup", () => {
+        if (passRef.value === "") {
+          passRef.style.borderBottomColor = "#293241";
+          PasswordField.style.color = "rgb(75 85 99 / var(--tw-text-opacity))";
+        } else if (passRegEx.test(passRef.value) === true) {
+          passRef.style.borderBottomColor = "red";
+          PasswordField.style.color = "red";
+        } else if (passRegEx.test(passRef.value) === false) {
+          passRef.style.borderBottomColor = "lightgreen";
+          PasswordField.style.color = "lightgreen";
+        }
+        if (passRef.value === "" || passConf.value === "")
+          passConf.style.borderBottomColor = "#293241";
+        else if (passConf.value !== passRef.value)
+          passConf.style.borderBottomColor = "red";
+        else if (passConf.value === passRef.value)
+          passConf.style.borderBottomColor = "lightgreen";
+      });
+
+      // pass confirmation
+      passConf.addEventListener("keyup", () => {
+        if (passConf.value === "" || passRef.value === "")
+          passConf.style.borderBottomColor = "#293241";
+        else if (passConf.value !== passRef.value) {
+          passConf.style.borderBottomColor = "red";
+        } else if (passConf.value === passRef.value) {
+          passConf.style.borderBottomColor = "lightgreen";
+        }
+      });
+
       form.addEventListener("submit", (e) => {
         e.preventDefault();
         const email = form.email.value;
         const pass = form.pass.value;
         const nume = form.nume.value;
         const prenume = form.prenume.value;
+
+        // verificari
+        if (email === "" || pass === "" || nume === "" || prenume === "") {
+          alert("completati campurile");
+          return;
+        } else if (passRegEx.test(pass) === true) {
+          alert("parola nu e safe");
+          return;
+        } else if (pass !== passConf.value) {
+          alert("nu e buna confirmarea parolei");
+          return;
+        }
+
         const credential = EmailAuthProvider.credential(email, pass);
         linkWithCredential(auth.currentUser, credential)
           .then((usercred) => {
             const UID = auth.currentUser.uid;
-            setDoc(doc(db, "pacienti", UID), {
-              nume,
-              prenume,
-            });
-            const user = usercred.user;
-            console.log(user);
+            const medicRef = doc(db, "invited_users", telefon);
+            getDuery(medicRef)
+              .then((cred) => {
+                const medic = cred;
+                setDoc(doc(db, "pacienti", UID), {
+                  nume,
+                  prenume,
+                  medic,
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            form.reset();
           })
           .catch((err) => {
             console.log(err);
