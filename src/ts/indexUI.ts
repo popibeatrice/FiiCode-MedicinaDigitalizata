c47a75e661757aa3614384f5cba0c75b5f8d2aac
popibeatrice <popicabeatrice31@gmail.com>
2023-03-03 20:35:28 +0200

    main medic

--- src/ts/indexUI.ts
+++ src/ts/indexUI.ts
@@ -15,13 +15,13 @@ const LastLogin = document.querySelector("#LastLogin") as HTMLFormElement;
 
 // Pop up
 joinBtn.addEventListener("click", (e) => {
-  joinPopup.classList.remove("hidden");
+  joinPopup.classList.remove("scale-0");
   joinPopup.classList.add("flex");
   joinWrap.classList.add("opacity-[65%]");
   joinWrap.classList.remove("pointer-events-none");
 });
 login_button.addEventListener("click", (e) => {
-  login_popup.classList.remove("hidden");
+  login_popup.classList.remove("scale-0");
   login_popup.classList.add("flex");
   wrap.classList.add("opacity-[65%]");
   wrap.classList.remove("pointer-events-none");
@@ -29,13 +29,13 @@ login_button.addEventListener("click", (e) => {
 
 // Pop up close
 joinWrap.addEventListener("click", (e) => {
-  joinPopup.classList.add("hidden");
+  joinPopup.classList.add("scale-0");
   joinPopup.classList.remove("flex");
   joinWrap.classList.remove("opacity-[65%]");
   joinWrap.classList.add("pointer-events-none");
 });
 wrap.addEventListener("click", (e) => {
-  login_popup.classList.add("hidden");
+  login_popup.classList.add("scale-0");
   login_popup.classList.remove("flex");
   wrap.classList.remove("opacity-[65%]");
   wrap.classList.add("pointer-events-none");
@@ -43,13 +43,13 @@ wrap.addEventListener("click", (e) => {
 
 // Pop up close
 joincloseBtn.addEventListener("click", (e) => {
-  joinPopup.classList.add("hidden");
+  joinPopup.classList.add("scale-0");
   joinPopup.classList.remove("flex");
   joinWrap.classList.remove("opacity-[65%]");
   joinWrap.classList.add("pointer-events-none");
 });
 close_login.addEventListener("click", (e) => {
-  login_popup.classList.add("hidden");
+  login_popup.classList.add("scale-0");
   login_popup.classList.remove("flex");
   wrap.classList.remove("opacity-[65%]");
   wrap.classList.add("pointer-events-none");
--- src/ts/medic/med_main.ts
+++ src/ts/medic/med_main.ts
@@ -1,2 +1,8 @@
 import "../../styles/medic/med_main.css";
 import "../../styles/index.css";
+
+const menuBtn = document.querySelector(".meniu");
+const navList = document.querySelector(".mobile-nav");
+menuBtn.addEventListener("click", (e) => {
+  menuBtn.classList.toggle("is-active");
+});
