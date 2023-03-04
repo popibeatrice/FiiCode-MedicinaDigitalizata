05d43d8af1a350338f3d6d38d11400d57e7cdce3
popibeatrice <popicabeatrice31@gmail.com>
2023-03-04 17:11:38 +0200

    sending invitation + first part of the main page

--- src/ts/medic/med_main.js
+++ src/ts/medic/med_main.js
@@ -4,16 +4,18 @@ import "./med_mainCanv";
 import { getDoc, doc, setDoc } from "firebase/firestore";
 import { db, auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
-const menuBtn = document.querySelector(".meniu");
-menuBtn.addEventListener("click", (e) => {
-  menuBtn.classList.toggle("is-active");
-});
 
+// veriables
+const menuBtn = document.querySelector(".meniu");
 const invitationBtn = document.querySelector("#invitation");
 const finalInvite = document.querySelector("#finalInvite");
 const PacientInvitation = document.querySelector(".PacientInvite");
 const inviteWrapper = document.querySelector(".invite-wrapper");
 const InviteForm = document.querySelector(".invite-form");
+const MeniuWrapper = document.querySelector(".meniu-wrapper");
+const CloseInvite = document.querySelector(".closeInvite-login");
+
+// async
 async function Invitation(param) {
   const docSnap = await getDoc(param);
   if (docSnap.exists()) {
@@ -22,6 +24,28 @@ async function Invitation(param) {
     return `${Nume} ${Prenume}`;
   } else throw TypeError("nu esti docos");
 }
+
+// events
+menuBtn.addEventListener("click", (e) => {
+  menuBtn.classList.toggle("is-active");
+  if (menuBtn.classList.contains("is-active")) {
+    MeniuWrapper.classList.add("opacity-[75%]");
+    MeniuWrapper.classList.remove("pointer-events-none");
+  } else {
+    MeniuWrapper.classList.remove("opacity-[75%]");
+    MeniuWrapper.classList.add("pointer-events-none");
+  }
+});
+MeniuWrapper.addEventListener("click", (e) => {
+  menuBtn.classList.remove("is-active");
+  MeniuWrapper.classList.remove("opacity-[75%]");
+  MeniuWrapper.classList.add("pointer-events-none");
+});
+CloseInvite.addEventListener("click", (e) => {
+  PacientInvitation.classList.add("scale-0");
+  inviteWrapper.classList.remove("opacity-[65%]");
+  inviteWrapper.classList.add("pointer-events-none");
+})
 
 invitationBtn.addEventListener("click", (e) => {
   PacientInvitation.classList.remove("scale-0");
@@ -43,7 +67,7 @@ InviteForm.addEventListener("submit", (e) => {
         user_email: InviteForm.email.value,
       };
       emailjs.send("service_r1dant5", "template_gjkxjna", params).then(() => {
-        console.log("succes");
+        alert("invitation mail was send to you pacient");
       });
       const UID = auth.currentUser.uid;
       setDoc(doc(db, "invited_users", InviteForm.phone.value), {
